01f02e2a708745c087f952a1d15e9fb6b4ecac99
David Henner <david.henner@epitech.eu>
2017-01-28 10:06:50 +0100

    Profile view controller start & QR Reader view controller start

---
+++ theux/QRReaderViewController.swift
@@ -0,0 +1,16 @@
+//
+//  QRReaderViewController.swift
+//  theux
+//
+//  Created by David Henner on 28/01/2017.
+//  Copyright © 2017 david. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class QRReaderViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
